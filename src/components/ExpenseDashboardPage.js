570936dd338a91fc9858eea90b5cdf3b2dac6f0a
Malgorzata Saczawa <msaczawa29@gmail.com>
2020-04-27 15:12:43 +0200

    text filtering

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
+import ExpenseListFilters from './ExpenseListFilters';
 
 const ExpenseDashboardPage = () => (
-    <div><ExpenseList/></div>
-  );
+  <div>
+    <ExpenseListFilters />
+    <ExpenseList />
+  </div>
+);
 
-  export default ExpenseDashboardPage
+export default ExpenseDashboardPage;
