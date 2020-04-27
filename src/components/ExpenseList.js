c5e91abecff3035f5788a67f54b635b9b5b19c45
Malgorzata Saczawa <msaczawa29@gmail.com>
2020-04-27 14:24:07 +0200

    rendering individual expense

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import ExpenseListItem from './ExpenseListItem';
 
 const ExpenseList = (props) => (
   <div>
     <h1>Expense List</h1>
-    {props.filters.text}
-    {props.expenses.length}
+    {props.expenses.map((expense) => {
+      return <ExpenseListItem key={expense.id} {...expense} />;
+    })}
   </div>
 );
 
 const mapStateToProps = (state) => {
   return {
     expenses: state.expenses,
-    filters: state.filters
+    filters: state.filters,
   };
 };
 
