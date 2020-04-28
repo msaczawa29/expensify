01ad7b51f0da52bb88766c93cec6efaf43ddf2f6
Malgorzata Saczawa <msaczawa29@gmail.com>
2020-04-28 19:53:55 +0200

    filtering expenses by dates

--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -1,10 +1,11 @@
+import moment from 'moment';
 // Filters Reducer
 
 const filtersReducerDefaultState = {
   text: '',
   sortBy: 'date',
-  startDate: undefined,
-  endDate: undefined
+  startDate: moment().startOf('month'),
+  endDate: moment().endOf('month'),
 };
 
 export default (state = filtersReducerDefaultState, action) => {
@@ -12,27 +13,27 @@ export default (state = filtersReducerDefaultState, action) => {
     case 'SET_TEXT_FILTER':
       return {
         ...state,
-        text: action.text
+        text: action.text,
       };
     case 'SORT_BY_AMOUNT':
       return {
         ...state,
-        sortBy: 'amount'
+        sortBy: 'amount',
       };
     case 'SORT_BY_DATE':
       return {
         ...state,
-        sortBy: 'date'
+        sortBy: 'date',
       };
     case 'SET_START_DATE':
       return {
         ...state,
-        startDate: action.startDate
+        startDate: action.startDate,
       };
     case 'SET_END_DATE':
       return {
         ...state,
-        endDate: action.endDate
+        endDate: action.endDate,
       };
     default:
       return state;
