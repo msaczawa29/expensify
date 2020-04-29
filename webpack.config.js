25069f91ce204dacf445dbd046cca688ceb91809
Malgorzata Saczawa <msaczawa29@gmail.com>
2020-04-29 18:04:44 +0200

    Mini Css Extract Plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = (env) => {
-const isProduction = env === 'production'
+  const isProduction = env === 'production';
 
   return {
     entry: './src/app.js',
@@ -18,11 +18,30 @@ const isProduction = env === 'production'
         },
         {
           test: /\.s?css$/,
-          use: ['style-loader', 'css-loader', 'sass-loader'],
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
+          ],
         },
       ],
     },
-    devtool:isProduction? 'source-map': 'cheap-module-eval-source-map',
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'styles.css',
+      }),
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true,
