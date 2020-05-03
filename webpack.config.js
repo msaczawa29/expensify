cc7cf1f721c8642bccb8d441d0c10df102b8cba6
Malgorzata Saczawa <msaczawa29@gmail.com>
2020-05-03 12:42:44 +0200

    adding loading page

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = (env) => {
   const isProduction = env === 'production';
 
   return {
-    entry: './src/app.js',
+    entry: ['babel-polyfill', './src/app.js'],
     output: {
       path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js',
@@ -45,7 +45,7 @@ module.exports = (env) => {
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true,
-      publicPath: '/dist/'
+      publicPath: '/dist/',
     },
   };
 };
